b942913aa5f70365a4b181ce09221f58fd8afd6f
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-11-18 11:51:18 -0500

    style: change spaces to tabs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,19 +3,19 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.jest,
-      },
-    },
-    rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-    },
-  },
+	{
+		files: ["**/*.{js,mjs,cjs}"],
+		plugins: { js },
+		extends: ["js/recommended"],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.jest,
+			},
+		},
+		rules: {
+			"no-unused-vars": "warn",
+			"no-undef": "warn",
+		},
+	},
 ]);
