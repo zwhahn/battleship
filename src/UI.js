2a2d482582b8994e9aaf8396883d57b69b73afbd
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-11-20 11:53:06 -0500

    style: add shipyard, change how grid borders are shown
    This maintains consistent sizing across the shipyard grid and board

--- src/UI.js
+++ src/UI.js
@@ -1,48 +1,97 @@
-import { Player } from "./battleship.js";
+import { Gameboard, Player } from "./battleship.js";
 import { Gameplay } from "./battleship.js";
 
 const player1 = new Player();
 const player2 = new Player();
 const game = new Gameplay(player1, player2);
-game.placeShipsRandomly();
 
 const playerBoards = document.getElementsByClassName("board-container");
 const playerOneBoard = playerBoards[0];
 const playerTwoBoard = playerBoards[1];
 
-// Player 1's Board
-for (let y = 0; y < 10; y++) {
-	for (let x = 0; x < 10; x++) {
-		const cell = document.createElement("div");
-		if (player1.board.gameboard[y][x] !== 0) {
-			cell.classList.add("ship");
-		}
-		cell.classList.add("cell");
-		playerOneBoard.appendChild(cell);
+const randomFleetBtn = document.querySelector(".random-fleet-btn");
+randomFleetBtn.addEventListener("click", () => {
+	player1.board = new Gameboard();
+	player2.board = new Gameboard();
+	game.placeShipsRandomly();
+	drawPlayer1Board();
+	drawPlayer2Board();
+});
 
-		cell.dataset.y = y;
-		cell.dataset.x = x;
+drawPlayer1Board();
+drawPlayer2Board();
+drawShipyard();
+
+function drawShipyard() {
+	const ships = [
+		{ name: "carrier", size: 5 },
+		{ name: "battleship", size: 4 },
+		{ name: "cruiser", size: 3 },
+		{ name: "submarine", size: 3 },
+		{ name: "destroyer", size: 2 },
+	];
+	const shipyard = document.querySelector(".shipyard");
+	ships.forEach((ship) => {
+		const shipDiv = document.createElement("div");
+		shipyard.appendChild(shipDiv);
+
+		shipDiv.classList.add("ship-piece");
+		shipDiv.draggable = true;
+		shipDiv.dataset.name = ship.name;
+		shipDiv.dataset.length = ship.size;
+
+		for (let i = 0; i < ship.size; i++) {
+			const cell = document.createElement("div");
+			cell.classList.add("cell");
+			shipDiv.appendChild(cell);
+		}
+	});
+}
+
+// Player 1's Board
+function drawPlayer1Board() {
+	playerOneBoard.innerHTML = "";
+
+	for (let y = 0; y < 10; y++) {
+		for (let x = 0; x < 10; x++) {
+			const cell = document.createElement("div");
+			if (player1.board.gameboard[y][x] !== 0) {
+				cell.classList.add("ship");
+			}
+			cell.classList.add("cell");
+			playerOneBoard.appendChild(cell);
+
+			cell.dataset.y = y;
+			cell.dataset.x = x;
+		}
 	}
 }
 
 // Player 2's Board
-for (let y = 0; y < 10; y++) {
-	for (let x = 0; x < 10; x++) {
-		const cell = document.createElement("div");
-		cell.classList.add("cell");
-		playerTwoBoard.appendChild(cell);
+function drawPlayer2Board() {
+	playerTwoBoard.innerHTML = "";
 
-		cell.dataset.y = y;
-		cell.dataset.x = x;
+	for (let y = 0; y < 10; y++) {
+		for (let x = 0; x < 10; x++) {
+			const cell = document.createElement("div");
+			if (player2.board.gameboard[y][x] !== 0) {
+				cell.classList.add("ship");
+			}
+			cell.classList.add("cell");
+			playerTwoBoard.appendChild(cell);
 
-		cell.addEventListener("click", (e) => {
-			const cx = e.target.dataset.x;
-			const cy = e.target.dataset.y;
+			cell.dataset.y = y;
+			cell.dataset.x = x;
 
-			game.handlePlayerMove(cy, cx);
-			updateCellColor(playerTwoBoard, cy, cx, player2.board);
-			computerMove();
-		});
+			cell.addEventListener("click", (e) => {
+				const cx = e.target.dataset.x;
+				const cy = e.target.dataset.y;
+
+				game.handlePlayerMove(cy, cx);
+				updateCellColor(playerTwoBoard, cy, cx, player2.board);
+				computerMove();
+			});
+		}
 	}
 }
 
